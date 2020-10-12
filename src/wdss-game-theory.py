1a119214ae288fe3fa2524372c53b234546215cc
THargreaves <tim.hargreaves@icloud.com>
2020-10-12 21:07:36 +0100

    Add base strategy and refine game class

--- src/wdss-game-theory.py
+++ src/wdss-game-theory.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 import signal
 import time
 
@@ -25,6 +26,7 @@ class Game:
 
         # Initialise game state
         self._scores = (0, 0)
+        self._turn = 0
         self._dq = (False, False)
         self._history = (
             {'action': [], 'intention': [], 'response': []},
@@ -41,7 +43,8 @@ class Game:
         self._simulate()
 
     def _simulate(self):
-        for i in range(self.iterations):
+        while self._turn < self._iterations:
+            self._turn += 1
             # Make requests to players
             self._t = tuple(self.time_allowance for __ in range(2))
             try:
@@ -54,7 +57,7 @@ class Game:
                 break
 
             # Update helper properties
-            self.update_honesty()
+            self._update_honesty()
 
             # Update scores
             if all(self._history[i]['action'][-1] == 0 for i in (0, 1)):
@@ -80,6 +83,33 @@ class Game:
             signal.alarm(0)
             self._t[i] -= (time.time() - start)
 
+    def _update_honesty(self):
+        # Honesty of players' intentions
+        for i in (0, 1):
+            if self._history[i]['intention'][-1] is None:
+                self._honesty[i]['intention'].append(None)
+            else:
+                self.honesty[i]['intention'].append(
+                    int(self.history[i]['action'][-1] ==
+                        self.history[i]['intention'][-1])
+                )
+
+        # Only update response honesty after second move
+        if not self._turn:
+            return
+
+        # Honesty of players' responses
+        for i in (0, 1):
+            j = i ^ 1
+            if self._history[i]['response'][-2] is None:
+                self._honesty[i]['response'].append(None)
+            else:
+                action = self._history[j]['action'][-2]
+                self._honesty[i]['response'].append(
+                    int(self._history[i]['action'][-1] ==
+                        self._history[i]['response'][-2][action])
+                )
+
     def _parse_attribute(self, attr, id_, type_):
         if id_ and type_:
             return getattr(self, attr)[id_][type_]
@@ -92,24 +122,22 @@ class Game:
 
     # ~~~ GETTER METHODS FOR PRIVATE ATTRIBUTES ~~~
 
-    @property
-    def iterations(self):
+    def get_iterations(self):
         return None if self.infinite else self._iterations
 
-    @property
-    def infinite(self):
+    def get_infinite(self):
         return self._infinite
 
-    @property
-    def scores(self, id_=None):
+    def get_scores(self, id_=None):
         return self._scores[id_] if id_ else self._scores
 
-    @property
-    def history(self, id_=None, type_=None):
+    def get_turn(self):
+        return self._turn
+
+    def get_history(self, id_=None, type_=None):
         return self._parse_attribute('_history', id_, type_)
 
-    @property
-    def honesty(self, id_=None, type_=None):
+    def get_honesty(self, id_=None, type_=None):
         return self._parse_attribute('_honesy', id_, type_)
 
     # ~~~ STATIC METHODS FOR VALIDATION AND ERROR HANDLING ~~~
@@ -135,6 +163,25 @@ class Game:
     @staticmethod
     def raiseTimeLimitError():
         raise TimeLimitError
+
+
+class BaseStrategy(ABC):
+
+    def __init__(self, game, id_):
+        # Assign to game and record ID
+        self.game = game
+        self.my_id = id_
+        self.op_id = id_ ^ 0
+
+    @abstractmethod
+    def state_action(self):
+        pass
+
+    def state_intention(self):
+        return None
+
+    def state_response(self):
+        return None
 
 
 class DisqualificationError(Exception):
